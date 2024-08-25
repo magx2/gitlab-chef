fe3eba0b60b5fb60ec7130071f8f364c65c0d1d8
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-08-25 17:45:27 +0200

    Add flex-wrap so buttons will wrap

--- app/content_scripts/chef.js
+++ app/content_scripts/chef.js
@@ -1,33 +1,4 @@
 (() => {
-    const recipes = [
-        {
-            name: "Auto Release",
-            variables: [
-                {key: "RELEASE_AUTO", value: "true"},
-                {key: "RELEASE_AUTO1", value: "true1"},
-                {key: "RELEASE_AUTO2", value: "true2"},
-            ]
-        },
-        {
-            name: "Onion",
-            emoji: "🧅",
-            variables: [
-                {key: "ME_NOT_LIKE", value: "tomato"},
-                {key: "ME_LIKE", value: "onion"},
-            ]
-        }
-    ]
-
-
-    async function findRecipe(name) {
-        for (const recipe of await allRecipes()) {
-            if (recipe.name === name) {
-                return recipe;
-            }
-        }
-        return null;
-    }
-
     async function allRecipes() {
         return (await browser.storage.local.get({
             recipes: []
@@ -58,20 +29,23 @@
         }
     }
 
-    // Remove all elements with the class 'chef-btn-recipe'
-    document.querySelectorAll('.chef-btn-recipe')
-        .forEach(element => element.remove());
 
-    // add buttons with recipes
-    const runPipelineButton = document.querySelector('[data-testid="run-pipeline-button"]');
-    if (runPipelineButton) {
-        const parentElement = runPipelineButton.parentElement;
-        let promise = allRecipes();
-        promise.then(recipes => {
-            recipes.forEach(recipe => {
+    async function init() {
+        // Remove all elements with the class 'chef-btn-recipe'
+        document.querySelectorAll('.chef-btn-recipe')
+            .forEach(element => element.remove());
+
+        // add buttons with recipes
+        document.querySelectorAll('.btn').forEach((button) => button.classList.add('gl-mt-3', 'gl-mr-3'))
+        const runPipelineButton = document.querySelector('[data-testid="run-pipeline-button"]');
+        if (runPipelineButton) {
+            const parentElement = runPipelineButton.parentElement;
+            parentElement.classList.add('flex-wrap');
+
+            (await allRecipes()).forEach(recipe => {
                 const newButton = document.createElement('btn');
                 //prepare
-                newButton.classList.add('btn', 'chef-btn-recipe', 'btn-default', 'btn-md', 'gl-button', 'gl-ml-3');
+                newButton.classList.add('btn', 'chef-btn-recipe', 'btn-default', 'btn-md', 'gl-button', 'gl-mr-3', 'gl-mt-3');
                 const span = document.createElement('span');
                 span.classList.add('gl-button-text');
                 const emoji = recipe.emoji || "👨‍🍳"
@@ -80,12 +54,13 @@
                 // on click
                 newButton.addEventListener('click', (event) => {
                     event.stopPropagation();
-                    recipeSelected(recipe).then(() => {
-                    });
+                    recipeSelected(recipe).then();
                 });
                 // append
                 parentElement.appendChild(newButton);
             })
-        });
+        }
     }
+
+    init().then()
 })();
